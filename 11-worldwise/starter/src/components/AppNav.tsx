34e5ad12daec7dff8359f61376cc60c3390ac223
Mateo Sánchez <mateosm@outlook.com>
2025-09-17 19:11:46 -0500

    feat: add logo to page nav, login and app cta

---
+++ 11-worldwise/starter/src/components/AppNav.tsx
@@ -0,0 +1,3 @@
+export function AppNav() {
+  return <nav>App Nav</nav>;
+}
---
+++ 11-worldwise/starter/src/pages/AppLayout.tsx
@@ -0,0 +1,10 @@
+import { AppNav } from "../components/AppNav";
+
+export function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  );
+}
