16619310082929b98e24c78c6265c2991e2b72c0
Mateo Sánchez <mateosm@outlook.com>
2025-10-18 10:33:12 -0500

    feat: edit app layout and add new components

--- 11-worldwise/starter/src/components/AppNav.tsx
+++ 11-worldwise/starter/src/components/AppNav.tsx
@@ -1,3 +1,17 @@
+import { NavLink } from "react-router-dom";
+import styles from "./AppNav.module.css";
+
 export function AppNav() {
-  return <nav>App Nav</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
